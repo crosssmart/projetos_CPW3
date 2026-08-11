4f50a308d97b174fff2bdaf8cd9dace8a744f91f
Rogerio Filho <rogerio.rhty@outlook.com>
2026-08-10 20:27:16 -0400

    v 0.01

--- aula00/backend/server.js
+++ aula00/backend/server.js
@@ -3,8 +3,29 @@ const app = express();
 
 //  define endpoint
 app.get('/', (req, res) => {
-  res.send('Hello World! Este é algo');
+  res.send('BEM VINDO!');
 });
+
+app.get('/aluno', (req, res) => {
+  res.send("ROTA OKAY");
+});
+
+// rota dinâmica (":" é uma variavel )
+app.get('/aluno/:nome', (req, res) => {
+  const nome = req.params.nome;
+  res.send(`Olá, ${nome}`);
+});
+
+// com número
+app.get('/soma/:a/:b', (req, res) => {
+  const a = Number(req.params.a);
+  const b = Number(req.params.b);
+  const resultado = a + b;
+  res.send(`Soma = ${resultado}!`);
+});
+
+
+
 
 // Liga o servidor pra escutar na porta 3000
 app.listen(3000, () => {
